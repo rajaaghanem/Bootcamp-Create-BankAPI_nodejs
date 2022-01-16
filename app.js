709d188c38ce43f28da11b66558eecef12eb2843
rajaaghanem <rajaaghanem@gmail.com>
2022-01-16 09:40:48 +0200

    add api to the urls

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ const utilis = require("./users.js");
 app.use(express.json());
 
 //return spicific user api by id
-app.get("/users/:id", (req, res) => {
+app.get("/api/users/:id", (req, res) => {
   res.send(utilis.readUser(req.params.id));
 });
 
 //return all users api
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   res.send(utilis.readAllUsers());
 });
 
 // add new user to the api
-app.post("/users", (req, res) => {
+app.post("/api/users", (req, res) => {
   res.send(utilis.addUser(req.body.cash, req.body.credit, req.body.passID));
 });
 
 // deposit cash to a user
-app.put("/users/:id", (req, res) => {
+app.put("/api/users/:id", (req, res) => {
   res.send(utilis.updateDeposit(req.params.id, req.body.amount));
 });
 
 // update a users credit
-app.put("/users/credit/:id", (req, res) => {
+app.put("/api/users/credit/:id", (req, res) => {
   res.send(utilis.updateCredit(req.params.id, req.body.amount));
 });
 
 // withdraw money from the user
-app.put("/users/withdraw/:id", (req, res) => {
+app.put("/api/users/withdraw/:id", (req, res) => {
   res.send(utilis.withdrawMoney(req.params.id, req.body.amount));
 });
 
 // transfer money from one user to another with credit
-app.put("/users/transfer/:id", (req, res) => {
+app.put("/api/users/transfer/:id", (req, res) => {
   res.send(utilis.transferMoney(req.params.id, req.body.id, req.body.amount));
 });
 
